151404dad37e66f468dee6b60ee4218483a02498
daunle <99643569+daunle@users.noreply.github.com>
2023-12-21 14:37:08 +0900

    Update merge.sh

--- T-Llama/merge.sh
+++ T-Llama/merge.sh
@@ -1,4 +1,4 @@
-python3 /home2/labhosik/llama/mllama/Chinese-LLaMA-Alpaca-main/scripts/merge_llama_with_chinese_lora.py \
+python3 merge_llama_with_T_lora.py \
     --base_model 'EleutherAI/polyglot-ko-1.3b' \
     --lora_model '/home2/labhosik/llama/mllama/model_output/pt_lora_model' \
     --output_dir /home2/labhosik/llama/mllama/model_output/pg_combined
